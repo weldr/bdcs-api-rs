612a03f787caf42f274c4236f5853831c3b56b52
David Shea <dshea@redhat.com>
2017-03-03 13:01:04 -0500

    Don't propagate units out of an Or expression.
    A branch of an Or can be false without making the whole expression
    false, and so we can't infer anything about units inside an Or in the
    context of the whole expression. When processing Or, propagate known
    values into it, but don't take anything back out.

--- src/depsolve.rs
+++ src/depsolve.rs
@@ -10,6 +10,25 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 pub fn unit_propagation(exprs: &mut Vec<Rc<RefCell<DepExpression>>>, assignments: &mut HashMap<DepAtom, bool>) -> bool {
+    unit_propagation_helper(exprs, assignments, true)
+}
+
+// if assign is true, when a unit is found, process it and add it to the cache so that it will be propagated
+// if assign is false, propagate units from the cache but do not add new units to the cache
+// In short, assign is false when processing an OR. Given an expression like:
+//
+//    And(A, Not(B), Or(A, And(D, C), E), Or(D, E))
+//
+// the first pass might result in something like:
+//
+//    A=true, B=false;
+//    And(Or(A, And(D, C), E), Or(D, E))
+//
+// Then, when processing the Or(A, And(D, C), E), the A can be removed because we know it's true,
+// but we shouldn't do anything with And(D, C), since messing with D will mess with the Or(D, E)
+// branch. The And(D, C) portion could be false and the parent Or() expression can still be true,
+// so don't try to infer anything from those values.
+fn unit_propagation_helper(exprs: &mut Vec<Rc<RefCell<DepExpression>>>, assignments: &mut HashMap<DepAtom, bool>, assign: bool) -> bool {
     let mut ever_changed = false;
 
     loop {
@@ -21,9 +40,11 @@ pub fn unit_propagation(exprs: &mut Vec<Rc<RefCell<DepExpression>>>, assignments
             match *(val.borrow_mut()) {
                 DepExpression::Atom(ref a) => {
                     if !assignments.contains_key(&a) {
-                        assignments.insert(a.clone(), true);
-                        changed = true;
-                        indices_to_remove.push(i);
+                        if assign {
+                            assignments.insert(a.clone(), true);
+                            changed = true;
+                            indices_to_remove.push(i);
+                        }
                     } else if assignments.get(&a) == Some(&true) {
                         changed = true;
                         indices_to_remove.push(i);
@@ -36,9 +57,11 @@ pub fn unit_propagation(exprs: &mut Vec<Rc<RefCell<DepExpression>>>, assignments
                     match *(rc.borrow()) {
                         DepExpression::Atom(ref a) => {
                             if !assignments.contains_key(&a) {
-                                assignments.insert(a.clone(), false);
-                                changed = true;
-                                indices_to_remove.push(i);
+                                if assign {
+                                    assignments.insert(a.clone(), false);
+                                    changed = true;
+                                    indices_to_remove.push(i);
+                                }
                             } else if assignments.get(&a) == Some(&false) {
                                 changed = true;
                                 indices_to_remove.push(i);
@@ -53,7 +76,7 @@ pub fn unit_propagation(exprs: &mut Vec<Rc<RefCell<DepExpression>>>, assignments
 
                 DepExpression::And(ref mut and_list) => {
                     // recurse on this list of expressions
-                    if unit_propagation(and_list, assignments) {
+                    if unit_propagation_helper(and_list, assignments, assign) {
                         changed = true;
                     }
 
@@ -69,7 +92,7 @@ pub fn unit_propagation(exprs: &mut Vec<Rc<RefCell<DepExpression>>>, assignments
                 },
 
                 DepExpression::Or(ref mut or_list) => {
-                    if unit_propagation(or_list, assignments) {
+                    if unit_propagation_helper(or_list, assignments, false) {
                         changed = true;
                     }
 
